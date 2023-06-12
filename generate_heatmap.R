fe1b33b3c26a147ac441c2c720769b0473e5a556
Song_lab <song_lab@Song-labui-MacBookPro.local>
2023-06-12 20:26:37 +0900

    row annotation added

--- generate_heatmap.R
+++ generate_heatmap.R
@@ -35,10 +35,16 @@ mat <- log10(mat)
 
 rownames(mat) = df$species
 
+row_ha = rowAnnotation(genus = df$genus,
+                       gp = gpar(col = "#c6c6c4"),
+                       annotation_name_gp= gpar(fontface = "bold"),
+                       annotation_legend_param = list(genus = list(at = unique(df$genus)))
+                       )
+
 png(args[3], width = 63.5, height = 40, units = "cm", res = 100)
 ht<- Heatmap(mat, name = "RPM (log 10)", col = col_fun, rect_gp = gpar(col = "#c6c6c4", lwd = 1), use_raster = TRUE, raster_device = "png",
         show_column_name = FALSE, show_row_dend = FALSE, show_column_dend = FALSE, row_names_side = "right", row_labels = rownames(mat), cluster_row_slices = FALSE, row_title = NULL,
-        column_order = colnames(mat), row_names_gp = gpar(fontface = "italic")
+        column_order = colnames(mat), left_annotation = row_ha, row_names_gp = gpar(fontface = "italic")
 )
 
 draw(ht)
